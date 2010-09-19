ab8549e4bf20227fcdd2737baa5660999e9457cb
Heiko W. Rupp <hwr@pilhuhn.de>
2010-09-19 22:25:34 +0200

    Add missing import

--- src/de/bsd/zwitscher/MainActivity.java
+++ src/de/bsd/zwitscher/MainActivity.java
@@ -7,6 +7,7 @@ import de.bsd.zwitscher.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
