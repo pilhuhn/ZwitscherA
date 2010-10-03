0c09a1b580631c3fda7b956917574306ce3a5f20
Heiko W. Rupp <hwr@pilhuhn.de>
2010-10-03 22:16:00 +0200

    Use xml declaration instead of new Listeners for onClick on buttons

--- src/de/bsd/zwitscher/OneTweetActivity.java
+++ src/de/bsd/zwitscher/OneTweetActivity.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 public class OneTweetActivity extends Activity {
 
 	Context ctx = this;
+	Status status ;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +35,7 @@ public class OneTweetActivity extends Activity {
 		setContentView(R.layout.single_tweet);
 		Bundle bundle = getIntent().getExtras();
 		if (bundle!=null) {
-			final Status status = (Status) bundle.get(getString(R.string.status));
+			status = (Status) bundle.get(getString(R.string.status));
 			Log.i("OneTweetActivity","Showing status: " + status.toString());
 						
 			TextView tv01 = (TextView) findViewById(R.id.TextView01);
@@ -77,51 +78,7 @@ public class OneTweetActivity extends Activity {
 
 			// -- now the buttons --
 
-			Button replyButton = (Button) findViewById(R.id.ReplyButton);
-			replyButton.setOnClickListener(new OnClickListener() {
 
-				@Override
-				public void onClick(View v) {
-					Intent i = new Intent(getApplicationContext(), NewTweetActivity.class);
-					i.putExtra(getString(R.string.status), status);
-					i.putExtra("op", getString(R.string.reply));
-					startActivity(i);
-
-				}
-			});
-			Button replyAllButton = (Button) findViewById(R.id.ReplyAllButton);
-			replyAllButton.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					Intent i = new Intent(getApplicationContext(), NewTweetActivity.class);
-					i.putExtra(getString(R.string.status), status);
-					i.putExtra("op", getString(R.string.replyall));
-					startActivity(i);
-
-				}
-			});
-
-			Button retweetButton = (Button) findViewById(R.id.RetweetButton);
-			retweetButton.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					th.retweet(status.getId());
-				}
-			});
-			Button classicRetweetButton = (Button) findViewById(R.id.ClassicRetweetButton);
-			classicRetweetButton.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					Intent i = new Intent(getApplicationContext(), NewTweetActivity.class);
-					i.putExtra(getString(R.string.status), status);
-					i.putExtra("op", getString(R.string.classicretweet));
-					startActivity(i);
-
-				}
-			});
 			Button threadButton = (Button) findViewById(R.id.ThreadButton);
 			if (status.getInReplyToScreenName()==null) {
 				threadButton.setEnabled(false);
@@ -157,74 +114,92 @@ public class OneTweetActivity extends Activity {
 					// TODO update button state
 				}
 			});
-
-			Button speakButton = (Button) findViewById(R.id.SpeakButton);
-			speakButton.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					TextToSpeech tts = new TextToSpeech(getApplicationContext(),new OnInitListener() {
-
-						@Override
-						public void onInit(int status) {
-							// TODO Auto-generated method stub
-
-						}
-						
-					});
-					tts.setOnUtteranceCompletedListener(new OnUtteranceCompletedListener() {
-						
-						@Override
-						public void onUtteranceCompleted(String utteranceId) {
-							System.out.println("Utterance done");
-							
-						}
-					});
-					tts.speak(status.getText(), TextToSpeech.QUEUE_ADD, null);
-
-				}
-			});
 			
-			Button translateButton = (Button) findViewById(R.id.TranslateButton);
-			translateButton.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					Translate.setHttpReferrer("http://bsd.de/zwitscher");
-					try {
-						// TODO get target language from system
-						String result = Translate.execute(status.getText(), Language.AUTO_DETECT, Language.GERMAN);
-						AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-						builder.setMessage(result);
-						builder.setTitle("Translation result");
-						builder.setNeutralButton("ok", new DialogInterface.OnClickListener() {
-							
-							@Override
-							public void onClick(DialogInterface dialog, int which) {
-								dialog.dismiss();
-							}
-						});
-						AlertDialog alert = builder.create();
-						alert.show();
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						Toast.makeText(getApplicationContext(), e.getMessage(), 15000).show();
-					}
-				}
-			});
-			
-
-
-			Button doneButton = (Button) findViewById(R.id.DoneButton);
-			doneButton.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					finish(); // finish the activity
-				}
-			});
-
 		}
+	}
+	
+	public void reply(View v) {
+		Intent i = new Intent(getApplicationContext(), NewTweetActivity.class);
+		i.putExtra(getString(R.string.status), status);
+		i.putExtra("op", getString(R.string.reply));
+		startActivity(i);
+
+	}
+	
+	public void replyAll(View v) {
+		Intent i = new Intent(getApplicationContext(), NewTweetActivity.class);
+		i.putExtra(getString(R.string.status), status);
+		i.putExtra("op", getString(R.string.replyall));
+		startActivity(i);
+
+	}
+
+
+	
+	public void retweet(View v) {
+		TwitterHelper th = new TwitterHelper(ctx);
+		th.retweet(status.getId());
+	}
+
+	
+	public void classicRetweet(View v) {
+		Intent i = new Intent(getApplicationContext(), NewTweetActivity.class);
+		i.putExtra(getString(R.string.status), status);
+		i.putExtra("op", getString(R.string.classicretweet));
+		startActivity(i);
+
+	}
+
+	
+	public void speak(View v) {
+		TextToSpeech tts = new TextToSpeech(getApplicationContext(),new OnInitListener() {
+
+			@Override
+			public void onInit(int status) {
+				// TODO Auto-generated method stub
+
+			}
+			
+		});
+		tts.setOnUtteranceCompletedListener(new OnUtteranceCompletedListener() {
+			
+			@Override
+			public void onUtteranceCompleted(String utteranceId) {
+				System.out.println("Utterance done");
+				
+			}
+		});
+		tts.speak(status.getText(), TextToSpeech.QUEUE_ADD, null);
+
+	}
+
+	
+	public void translate(View v) {
+		Translate.setHttpReferrer("http://bsd.de/zwitscher");
+		try {
+			// TODO get target language from system
+			String result = Translate.execute(status.getText(), Language.AUTO_DETECT, Language.GERMAN);
+			AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+			builder.setMessage(result);
+			builder.setTitle("Translation result");
+			builder.setNeutralButton("ok", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+			AlertDialog alert = builder.create();
+			alert.show();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Toast.makeText(getApplicationContext(), e.getMessage(), 15000).show();
+		}
+	}
+
+	
+	public void done(View v) {
+		finish();
 	}
 }
