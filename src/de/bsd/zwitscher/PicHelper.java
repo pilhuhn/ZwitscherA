7c025c5ef3dc98a2e68bc7f5100ba31dc86c6b9f
Heiko W. Rupp <hwr@pilhuhn.de>
2010-10-14 22:11:09 +0200

    Refactor

--- src/de/bsd/zwitscher/PicHelper.java
+++ src/de/bsd/zwitscher/PicHelper.java
@@ -21,29 +21,21 @@ public class PicHelper {
 
 	public Bitmap getUserPic(User user,Context context) {
 
-		// TODO optimize see e.g. http://www.androidpeople.com/android-load-image-from-url-example/#comment-388
-
 		URL imageUrl = user.getProfileImageURL();
 
 		String username = user.getScreenName();
         Log.i("getUserPic","Looking for pic of user '" + username + "' from '" + imageUrl.toString() + "'");
 		boolean found = false;
+		// TODO use v8 methods when we require v8 in the manifest. Is probably too early yet.
 		String externalStorageState = Environment.getExternalStorageState();
 		try {
 			if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
-				File baseDir = Environment.getExternalStorageDirectory();
-				File iconDir = new File(baseDir,"/Android/data/de.bsd.zwitscher/files/user_profiles");
-				if (!iconDir.exists())
-					iconDir.mkdirs();
-				File iconFile = new File(iconDir,username);
+				File iconFile = getPictureFileForUser(username);
 				if (iconFile.exists() && iconFile.lastModified() > System.currentTimeMillis() - ONE_DAY)
 					found = true;
 			} 
-			
-//			FileInputStream fis = context.openFileInput(username);
-//			found = true;
-//			fis.close();
-            Log.i("getUserPic","Picture was on file system");
+			if (found)
+				Log.i("getUserPic","Picture was on file system");
 		}
 		catch (Exception ioe) {
 			Log.i("PicHelper", ioe.getMessage());
@@ -54,13 +46,8 @@ public class PicHelper {
                 Log.i("getUserPic","Downloading image and persisting it locally");
                 BufferedInputStream in = new BufferedInputStream(imageUrl.openStream());
     			if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
-    				File baseDir = Environment.getExternalStorageDirectory();
-    				File iconDir = new File(baseDir,"/Android/data/de.bsd.zwitscher/files/user_profiles");
-    				if (!iconDir.exists())
-    					iconDir.mkdirs();
-    				File iconFile = new File(iconDir,username);
+    				File iconFile = getPictureFileForUser(username);
     				BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(iconFile));
-//                BufferedOutputStream out = new BufferedOutputStream(context.openFileOutput(username,Context.MODE_WORLD_READABLE));
                 int val;
                 while ((val = in.read()) > -1)
                     out.write(val);
@@ -78,9 +65,7 @@ public class PicHelper {
 		try {
             Log.i("getUserPic","creating bitmap");
 			if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
-				File baseDir = Environment.getExternalStorageDirectory();
-				File iconDir = new File(baseDir,"/Android/data/de.bsd.zwitscher/files/user_profiles");
-				File iconFile = new File(iconDir,username);
+				File iconFile = getPictureFileForUser(username);
 				FileInputStream fis = new FileInputStream(iconFile);
 			Bitmap bi = BitmapFactory.decodeStream(fis);
 			fis.close();
@@ -91,5 +76,14 @@ public class PicHelper {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	private File getPictureFileForUser(String username) {
+		File baseDir = Environment.getExternalStorageDirectory();
+		File iconDir = new File(baseDir,"/Android/data/de.bsd.zwitscher/files/user_profiles");
+		if (!iconDir.exists())
+			iconDir.mkdirs();
+		File iconFile = new File(iconDir,username);
+		return iconFile;
 	}
 }
