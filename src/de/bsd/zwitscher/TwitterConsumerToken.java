d76fb600bd41137b24178ec241de6f052633bc40
Heiko W. Rupp <hwr@pilhuhn.de>
2011-06-02 14:34:50 +0200

    GH-5 Provide a consumer token with dummy values

---
+++ src/de/bsd/zwitscher/TwitterConsumerToken.java
@@ -0,0 +1,16 @@
+package de.bsd.zwitscher;
+
+/**
+ * Class that holds the twitter consumer token
+ */
+public class TwitterConsumerToken {
+
+	public static String consumerKey = "-replace me-";
+	public static String consumerSecret = "-replace -me";
+
+    // true for identi.ca ; disable if your twitter oauth token is not enabled for xAuth
+    // future version of the code will have a changed logic.
+    public static boolean xAuthEnabled = true;
+
+
+}
