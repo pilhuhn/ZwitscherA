c38ecf549fd2b32520b22c2dbea4039987149087
Heiko W. Rupp <hwr@pilhuhn.de>
2013-09-30 20:51:43 +0200

    Prevent a crash if bundle is null. Related to GH-162

--- src/de/bsd/zwitscher/OneTweetActivity.java
+++ src/de/bsd/zwitscher/OneTweetActivity.java
@@ -143,7 +143,9 @@ public class OneTweetActivity extends Activity implements OnInitListener, OnUtte
 
         } else {
             // Called from within Zwitscher
-            status = (Status) bundle.get(getString(R.string.status));
+            if (bundle!=null) {
+                status = (Status) bundle.get("status"); // NO-NLS
+            }
         }
 
 
